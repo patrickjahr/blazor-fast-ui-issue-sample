6134fe835342081978e245d44e7eb0dd27ab1c8b
Patrick Jahr <patrick.jahr@googlemail.com>
2023-04-18 12:48:20 +0200

    initial commit

---
+++ FastUi.Sample.Components/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace FastUi.Sample.Components
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCustomLib(this IServiceCollection services)
+        {
+            LibraryConfiguration config = new(ConfigurationGenerator.GetIconConfiguration(), ConfigurationGenerator.GetEmojiConfiguration());
+            services.AddFluentUIComponents(config);
+        }
+    }
+}
---
+++ FastUi.Sample.Components/wwwroot/FastUi.Sample.Components.lib.module.js
@@ -0,0 +1,16 @@
+﻿export function afterStarted(blazor) {
+    var rebootLink = document.createElement('link');
+    rebootLink.rel = 'stylesheet';
+    rebootLink.setAttribute('href', '_content/Microsoft.Fast.Components.FluentUI/css/reboot.css');
+    document.head.appendChild(rebootLink);
+
+    var variablesLink = document.createElement('link');
+    variablesLink.rel = 'stylesheet';
+    variablesLink.setAttribute('href', '_content/Microsoft.Fast.Components.FluentUI/css/variables.css');
+    document.head.appendChild(variablesLink);
+
+    var customScript = document.createElement('script');
+    customScript.type = 'module';
+    customScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/@fluentui/web-components/dist/web-components.min.js');
+    document.body.appendChild(customScript);
+}
